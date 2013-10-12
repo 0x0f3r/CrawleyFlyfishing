00f66976218fb1516c66124cfa6f9e4d190b8f35
Tatskaari <jony1710@hotmail.co.uk>
2013-10-12 22:19:34 +0100

    Added some basic styling to the admin page.

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,32 @@
-<h3>Admin site</h3>
-<p>This is the admin site. You should need admin privlages to get here.
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Admin - Crawley Fly Fishing</title>
+	</head>
+	<link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+	<body>
+		<div id="header">
+			<br>
+			<div>
+				<h1>Administration Control Panel</h1>
+			</div>
+			<a class="pull-right">Back to main site</a>
+		</div>
+		<div id="content">
+			<div class="pull-left" id="navbar">
+				<ul>
+					<li><a>new post</a></li>
+					<li><a>view posts</a></li>
+					<li><a>calendar</a></li>
+					<li><a>help</a></li>
+				</ul>
+			</div>
+			<div id="page-content">
+				This is some page content.
+			</div>
+		</div>
+	</body>
+</html>

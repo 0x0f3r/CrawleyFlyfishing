76bb9680ea032ecb931f48576c91feecbaa4ac54
Tatskaari <jony1710@hotmail.co.uk>
2013-10-12 02:51:51 +0100

    Removed the broken second slide reel from the welcome page.

--- welcome.php
+++ welcome.php
@@ -1,3 +1,1 @@
 <iframe seamless scrolling="no" src="slide-real/index.php" style="width: 100%; height: 500px; margin: 0 auto;"></iframe>
-<br>
-<iframe seamless scrolling="no" src="slide-real/index.php" style="width: 100%; height: 500px; margin: 0 auto;"></iframe>

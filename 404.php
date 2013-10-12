e32f66aba492e1ee15aff4b9da3c7bb15b43f7f1
Tatskaari <jony1710@hotmail.co.uk>
2013-10-12 02:39:19 +0100

    First commit of a file

---
+++ 404.php
@@ -0,0 +1,2 @@
+<h2>404 - Page not found!</h2>
+<p> Sorry but the page that you are looking for does noe exist. This could be because you made an error when entering the URL or the page has not been made yet. This website is still under construction after all.

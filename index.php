568dc369374595ca0b938f542f5ad1271d5c98e0
Tatskaari <jony1710@hotmail.co.uk>
2013-10-12 22:20:27 +0100

    modified the layout of the links in the header so they are always gold and changed the admin link at the bottom of the page to link to the site in the new way.

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <meta charset="utf-8" />
 <title>Crawley Fly Fishing</title>
 <!-- Bootstrap -->
-		<link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="css/layout.css" media="screen">
 		
 <!-- JQ UI -->
@@ -35,21 +35,23 @@
 	</div>
 	<div id="inner_content" class="rounded-container">
 		<?php 
-			if (strpos($_GET["page"], "admin") !== false) {
-				include("403.php");
-			}else if(array_key_exists("page", $_GET) && file_exists ($_GET["page"])){
-				include($_GET["page"]); 
-			}else if(!array_key_exists("page", $_GET)){
-				include("welcome.php");
+			if(array_key_exists("page", $_GET) ){
+				if (strpos($_GET["page"], "admin") !== false) {
+					include("403.php");
+				}else if(file_exists ($_GET["page"])){
+					include($_GET["page"]); 
+				}else{
+					include("404.php");
+				}
 			}else{
-				include("404.php");
+				include("welcome.php");
 			}
 		?>
 		<div style="clear: both"></div>
 	</div>
 	<div id="footer">
 		<a href=?page=welcome.php><img src="flyfisherslogo.png" alt="logo"/></a>
-		<div class="pull-right" style="position: relative; top: 90%;"><a href="?page=admin/index.php"><small>Admin</small></a></div>
+		<div class="pull-right" style="position: relative; top: 90%;"><a href="admin/index.php"><small>Admin</small></a></div>
 		<p>Boring footer stuff! This is where you might put legal stuff.<br>
 		   Made by Jonathan Poole. <br>
 		   As a pet project to learn more about web development. <br>

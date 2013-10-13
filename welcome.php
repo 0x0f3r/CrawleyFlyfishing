8b1ad2dc5868e76ac5c28fdf439dc702d67d0191
Tatskaari <jony1710@hotmail.co.uk>
2013-10-13 01:48:19 +0100

    Added images to the slide show and changed the colour of the gallery link box.

--- welcome.php
+++ welcome.php
@@ -19,7 +19,6 @@
     position: relative;
     min-height: 100%;
     width: 100%;
-    top: -25%;
   }
   div .link-box{
     width: 50%;
@@ -42,22 +41,28 @@
     <li data-target="#slider" data-slide-to="0" class="active"></li>
     <li data-target="#slider" data-slide-to="1"></li>
     <li data-target="#slider" data-slide-to="2"></li>
+    <li data-target="#slider" data-slide-to="3"></li>
   </ol>
   <!-- Carousel items -->
   <div class="carousel-inner">
     <div class="active item">
       <div>
-        <img src="http://i.imgur.com/rOzZets.jpg">
+        <img src="image/slide1.JPG" style="top: -25%;">
       </div>
     </div>
     <div class="item">
       <div>
-        <img src="http://i.imgur.com/2Q6OdQD.jpg">
+        <img src="image/slide2.JPG" style="top: -25%;">
       </div>
     </div>
     <div class="item">
       <div>
-        <img src="http://i.imgur.com/U2zcA2H.jpg">
+        <img src="image/slide3.JPG" style="top: -150%;">
+      </div>
+    </div>
+    <div class="item">
+      <div>
+        <img src="image/slide4.JPG" style="top: -25%;">
       </div>
     </div>
   </div>
@@ -67,7 +72,7 @@
   <a class="carousel-control left" href="#slider" data-slide="prev">&lsaquo;</a>
   <a class="carousel-control right" href="#slider" data-slide="next">&rsaquo;</a>
 </div>
-<a href="?page=gallery.php"><div class="link-box pull-left" style="background-color: #70AAAA;"><img class="pull-right" src="camera.png"><h2>Gallery</h2></div></a>
+<a href="?page=gallery.php"><div class="link-box pull-left" style="background-color: #A1BE86;"><img class="pull-right" src="camera.png"><h2>Gallery</h2></div></a>
 <a href="?page=newsletter.php"><div class="link-box pull-right" style="background-color: #086608;"><img class="pull-right" src="mail.png"><h2>Newsletter</h2></div></a>
 <h3>Welcome, have a look around</h3>
 <p>Crawley fly fishing club is a group of friendly fishermen and fisherwomen passinate about fly fishing. 

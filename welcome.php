d74cda0f4cf67ff60979b00ad92a66e5310e0d18
Tatskaari <jony1710@hotmail.co.uk>
2013-10-13 01:24:17 +0100

    Minor style changes to the welcome page boxes.

--- welcome.php
+++ welcome.php
@@ -68,6 +68,6 @@
   <a class="carousel-control right" href="#slider" data-slide="next">&rsaquo;</a>
 </div>
 <a href="?page=gallery.php"><div class="link-box pull-left" style="background-color: #70AAAA;"><img class="pull-right" src="camera.png"><h2>Gallery</h2></div></a>
-<a href="?page=newsletter.php"><div class="link-box pull-right" style="background-color: #70AA30;"><img class="pull-right" src="mail.png"><h2>Newsletter</h2></div></a>
+<a href="?page=newsletter.php"><div class="link-box pull-right" style="background-color: #086608;"><img class="pull-right" src="mail.png"><h2>Newsletter</h2></div></a>
 <h3>Welcome, have a look around</h3>
 <p>Crawley fly fishing club is a group of friendly fishermen and fisherwomen passinate about fly fishing. 

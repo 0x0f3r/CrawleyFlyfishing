4769f891d5a4a412932aea31eea81675558be61e
Tatskaari <jony1710@hotmail.co.uk>
2013-10-14 22:33:42 +0100

    Made the image viewer work on the gallery page. You can click on the images and a larger one will now be displayed in full screen. Looking into making the image scale better when resizing screen.

--- gallery.php
+++ gallery.php
@@ -2,14 +2,78 @@
 	#images img{
 		height: 150px;
 		margin: 1em;
+		cursor: pointer;
+	}
+	#backdrop{
+		background-color: black;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 1000;
+		opacity: 0.8;
+		display: none;
 	}
 	#image-viewer{
 		display: none;
-		height: 90%;
+
 		position: fixed;
+		left: 5%;
 		top: 5%;
+
+		width: 90%;
+		height: 90%;
+
+		background-color: black;
+
+		z-index: 1001;
+	}
+	#image-viewer-overflow{
+		overflow: hidden;
+		width: 100%;
+		height: 100%;
+	}
+	#image-viewer img{
+		margin: 0 auto;
+		display: block;
+		height: 100%;
+		width: auto;
+	}
+	#image-viewer-close{
+		position: absolute;
+		right: -10px;
+		top: -10px;
+		width: 20px;
+		height: 20px;
+
+		-webkit-border-radius: 999px;
+	    -moz-border-radius: 999px;
+	    border-radius: 999px;
+	    behavior: url(PIE.htc);
+
+	    background-color: black;
+	    border: 1px solid white;
+
+	    color: white;
+	    text-align: center;
+
+	    cursor: pointer;
 	}
 </style>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#images img").click(function(){
+			$("#image-viewer").css({display : "table-cell"});
+			$("#backdrop").css({display: "block"});
+			$("#image-viewer-image").attr("src", $(this).attr("src"))	
+		});
+		$("#backdrop, #image-viewer-close").click(function(){
+			$("#image-viewer").css({display : "none"});
+			$("#backdrop").css({display: "none"});
+		});
+	});
+</script>
 <div id="images">
 	<?php 
 		$finfo = finfo_open(FILEINFO_MIME_TYPE);
@@ -29,4 +93,7 @@
 	?>
 </div>
 <div id="image-viewer">
+	<div id="image-viewer-close"> X</div>
+	<div id="image-viewer-overflow"><img id="image-viewer-image" alt="Image viewer image" src=""></div>
 </div>
+<div id="backdrop"></div>

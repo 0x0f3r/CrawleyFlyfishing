1da8f5e3b2967ed755acf6d6a2984eec8a0976cf
Tatskaari <jony1710@hotmail.co.uk>
2014-01-04 03:30:46 +0000

    Added page pagination Implemented pagination to the newsletter. It will no longer just display all the posts but rather two at a time.This is just a number for testing. The final product will likely display 5 at a time.

--- newsletter.php
+++ newsletter.php
@@ -1,22 +1,69 @@
-<br>
-<div style="clear: both; margin-bottom: 3px;"></div>
 <div id="news">
 	<?php
 		$dir = "./newsletters/";
 		$files = scandir($dir, 1);
-		$fileCount = count($files);
-		
-		//loop through all the posts 
-		for($i = 0; $i < ($fileCount - 3); $i++){
-			echo "<div class='news-post'>";
-			include($dir . $files[$i]); 
-			echo " <div style='clear: both'></div></div> ";
+		$fileCount = count($files) - 2;
+		$pageLength = 2;
+		$pageCount = ceil($fileCount/$pageLength);
+
+		$page = 0;
+		if(isset($_GET['p']) && ctype_digit($_GET['p'])){
+			$page = $_GET["p"];
+
+			//if the page is out of bounds then redirect them to the last page
+			if($page >= $pageCount){
+				$page = $pageCount - 1;
+				header('Location: ?page=newsletter.php&p=' . $page);
+			}
 		}
 		
-		//Add the first (earliest) post with the css styling to add the bottom border.
-		echo "<div class='first-post news-post'>";
-		include($dir . $files[$fileCount - 3]); 
-		echo "<div style='clear: both'></div></div>  ";
+		echoPageNumbers($pageCount, $page);
+
+		//loop through all the posts 
+		for($i = $page * $pageLength;; $i++){
+			if($i < (($page * $pageLength) + $pageLength-1) && $i < $fileCount-1){
+				echo "<div class='news-post'>";
+				include($dir . $files[$i]); 
+				echo " <div style='clear: both'></div></div> ";
+			}else{
+				echo "<div class='first-post news-post'>";
+				include($dir . $files[$i]); 
+				echo "<div style='clear: both'></div></div>  ";
+				break;
+			}
+		}
+
+		echoPageNumbers($pageCount, $page);
+
+		function echoPageNumbers($pageCount, $page){
+			$startPage=0;
+			$endPage = 5;
+
+			if($page >= 2){
+				$startPage = $page - 2;
+				$endPage = $page + 3;
+			}
+			if($endPage > $pageCount){
+				$endPage = $pageCount;
+				$startPage = $endPage - 5;
+			}
+			if($startPage < 0){
+				$startPage = 0;
+			}
+
+			echo '<div class="pagination pull-right" style="margin: 6px 2em 0 0"><ul>';
+			echo '<li><a href="?page=newsletter.php&p=0">«</a></li>';
+			for($i = $startPage; $i < $endPage; $i++){
+				if($i == $page){
+					echo '<li class="active">';
+				}else{
+					echo '<li>';
+				}
+				echo('<a href="?page=newsletter.php&p=' . $i . '">' . ($i + 1) . '</a></li>');
+			}
+			echo '<li><a href="?page=newsletter.php&p=' . ($pageCount-1) . '">»</a></li>';
+			echo '</ul></div>';
+			echo '<div style="clear:both"></div>';
+		}
 	?>
 </div>
-<br>
